88a514c8ea901f2120fb98ce4dd40e0bd3546cf9
Juan Ramirez <rami8224.2007@gmail.com>
2022-03-28 11:39:13 -0400

    completed part1

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+//part 1 ---------------- ALERTER
+let textBox = document.querySelector('#text-box')
+
+let submitButton = document.querySelector('#submit-button')
+console.log(textBox)
+//access value of forms using value
+submitButton.addEventListener('click',()=> {
+    alert(textBox.value)
+})
+//-------------------------------------
+
+//part 2 ----------------- Sandwich builder
+
+
+//----------------------------
