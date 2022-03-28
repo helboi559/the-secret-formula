0f9ffa5d7cb126ea545b6ca36384df7805653a13
Juan Ramirez <rami8224.2007@gmail.com>
2022-03-28 16:50:21 -0400

    completed goal 4

--- main.js
+++ main.js
@@ -27,5 +27,46 @@ makeSandwich.addEventListener("click", (event)=> {
 
     alert(message)
 })
-
 //----------------------------
+
+//part3 ---------------------Haircut appointment 
+let date = document.querySelector('#date')
+let barber = document.querySelector('#barber')
+let length = document.querySelectorAll('input[name="length"]')
+let makeAppt = document.querySelector('#makeAppt')
+// console.log(length)
+makeAppt.addEventListener('click',()=> {
+    let whatLen = ""
+    for(let len of length) {
+        if (len.checked) {
+            whatLen += len.value
+        }
+    }
+    let msg = `Haircut scheduled for ${date.value} with ${barber.value} for ${whatLen} hair.`
+    alert(msg)
+})
+
+//--------------------------
+//part 4 -------------------create account!
+
+let user = document.querySelector('#user')
+let email = document.querySelector('#email')
+let pw = document.querySelector('#pw')
+let confirmPw = document.querySelector('#confirm-pw')
+let makePw = document.querySelector('#makepw')
+console.log(makePw)
+makePw.addEventListener('click',()=> {
+    // let msg = ""
+    // console.log(pw.value.length)
+    
+    if(pw.value === confirmPw.value) {
+        pw.value= '**********'
+        let msg = `Your user email is: ${email.value} and your password is: ${pw.value}. ACCOUNT CREATED!`
+        alert(msg)
+    } else {
+        alert(`Your username and password dont match! Try again.`)
+    }
+    
+})
+
+//-------------------------------------------
