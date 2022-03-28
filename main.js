6ff663c9737bbf9763c48644a8ba895d0bd2a509
Juan Ramirez <rami8224.2007@gmail.com>
2022-03-28 15:08:22 -0400

    completed part-2

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 let textBox = document.querySelector('#text-box')
 
 let submitButton = document.querySelector('#submit-button')
-console.log(textBox)
+// console.log(textBox)
 //access value of forms using value
 submitButton.addEventListener('click',()=> {
     alert(textBox.value)
@@ -12,6 +12,20 @@ submitButton.addEventListener('click',()=> {
 //-------------------------------------
 
 //part 2 ----------------- Sandwich builder
+//identify button/"boxlist"
+let makeSandwich = document.querySelector('#make-sandwich input')
+let boxes = document.querySelectorAll('input[name="ingredient"]')
+// console.log(boxes)
+makeSandwich.addEventListener("click", (event)=> {
+    event.preventDefault();
+    let message = "Your sandwich contains: "
+    for(let box of boxes) {
+        if(box.checked) {
+            message += `${box.value}, `
+        }
+    }
 
+    alert(message)
+})
 
 //----------------------------
